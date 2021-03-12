9ac96c7c3cc9d573ee9a9554681a880113b4df4d
syd333 <sydmiranda@icloud.com>
2021-03-12 11:35:39 -0800

    commit tree dcf0e74bdb4ed2c088463e9676bce78f7595ff1a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('bathe my dog!')
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork('work from home.')
+
+function wrapAdjective(){
+    
+}
+
+let Calculator = {
+    add(x, y){
+        return x + y
+    },
+    subtract(x, y){
+        return x - y
+    },
+    multiply(x, y){
+        return x * y
+    },
+    divide(x, y){
+        return x / y
+    }
+}
+Calculator.add(1,3)
+Calculator.subtract(1,3)
+Calculator.multiply(1,3)
+Calculator.divide(10,5)
+
+function actionApplyer(num, array) {
+    return array[num]
+}
